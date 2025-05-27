9b7c6fd631b41ec876ea842f85e210a7bfeb3dd7
Yoru <dan.bingchenwu@gis.tw>
2025-05-27 17:12:59 +0800

    upload_function

--- Split_Traget_Video_detection.py
+++ Split_Traget_Video_detection.py
@@ -429,7 +429,7 @@ class YouTubeObjectDetector:
 if __name__ == "__main__":
     Video_path = './TestVideo/'
     model_list = ['ppe.pt']
-    video_select = ['35-1','35-2']
+    video_select = [1,2,3]
 
     for modelselect in model_list:
         print(f"\n----- 開始處理模型：{modelselect} -----")
