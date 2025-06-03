00549e43984d6d1b41ac20a5f5d61400be2c09f4
Yoru <dan.bingchenwu@gis.tw>
2025-06-03 11:06:15 +0800

    Label_Img_V2

--- Split_Traget_Video_detection_V2.py
+++ Split_Traget_Video_detection_V2.py
@@ -195,8 +195,10 @@ class YouTubeObjectDetector:
                     class_name = self.model.names[cls]
                     if class_name in ['Hardhat', 'NO-Hardhat']:
                         class_id = 0
-                    elif class_name in ['Safety Vest', 'NO-Safety Vest']:
+                    elif class_name in ['Safety Vest']:
                         class_id = 7
+                    elif class_name in ['NO-Safety Vest']:
+                        class_id = 4
                     else:
                         continue
                     
@@ -384,7 +386,7 @@ class YouTubeObjectDetector:
 if __name__ == "__main__":
     Video_path = './TestVideo/'
     model_list = ['ppe.pt']
-    video_select = [1,2,3]
+    video_select = ['35-1','35-2']
 
     for modelselect in model_list:
         print(f"\n----- 開始處理模型：{modelselect} -----")
